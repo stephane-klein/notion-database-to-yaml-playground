87a97090775c93d1381bd57323337b33600899af
Stéphane Klein <contact@stephane-klein.info>
2023-10-26 15:22:32 +0200

    Add page pagination

--- playground.js
+++ playground.js
@@ -6,11 +6,12 @@ const notion = new Client({
     auth: process.env.NOTION_TOKEN,
 })
 
-console.dir(
-    await notion.databases.query({
-        // https://www.notion.so/61b0ae8cade84ef08870ca24680fd605?v=c6f401cc136346959ad031eb4dede3b7
-        database_id: "61b0ae8cade84ef08870ca24680fd605",
-    }),
-    { depth: null }
-);
+let databaseResult = await notion.databases.query({
+    // https://www.notion.so/61b0ae8cade84ef08870ca24680fd605?v=c6f401cc136346959ad031eb4dede3b7
+    database_id: "61b0ae8cade84ef08870ca24680fd605"
+});
 
+for (const page of databaseResult.results) {
+    console.log("---");
+    console.dir(page, { depth: null });
+}
